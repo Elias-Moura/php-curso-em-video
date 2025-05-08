3bfed6b1127fc514274be2acb85f8a9479b2b88e
elias-moura <elias.smourayt@gmail.com>
2025-05-08 15:55:55 -0300

    variaveis e consts

---
+++ src/2-variaveis-e-constantes/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        // Variáveis são declaradas com $
+        $nome = "Elias";
+        $idade = 24;
+        // const são declaradas com a palavra chave const
+        const PAIS = "Brasil";
+            
+        print "$nome tem $idade de idade. Mora no $PAIS";
+    ?>
+</body>
+</html>
